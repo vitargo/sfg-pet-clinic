e2bdae8c618afca7c8d1427bd1c2dd76a749179c
viktoriia.redko <viktoriia.redko@ericsson.com>
2022-06-27 12:19:19 +0300

    Initial Project Add

---
+++ src/main/java/org/vitargo/vspetclinic/VsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package org.vitargo.vspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/vitargo/vspetclinic/VsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vitargo.vspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
