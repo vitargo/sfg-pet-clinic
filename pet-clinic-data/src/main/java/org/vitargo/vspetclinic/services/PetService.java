2cce0a53f9040b00bf5c137a88804be78bb0dc10
viktoriia.redko <viktoriia.redko@ericsson.com>
2022-07-14 15:25:30 +0300

    Implement interfaces fot services

---
+++ pet-clinic-data/src/main/java/org/vitargo/vspetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package org.vitargo.vspetclinic.services;
+
+import org.vitargo.vspetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
