017500e67851e0d8fc33d845c0e6f29d89ecc369
viktoriia.redko <viktoriia.redko@ericsson.com>
2022-08-25 17:03:57 +0300

    Add junit test

--- pet-clinic-web/src/test/java/org/vitargo/vspetclinic/VsPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/org/vitargo/vspetclinic/VsPetClinicApplicationTest.java
@@ -1,7 +1,17 @@
 package org.vitargo.vspetclinic;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
 class VsPetClinicApplicationTest {
+
+    @Test
+    void contextLoaderTest(){
+
+    }
 
 }
